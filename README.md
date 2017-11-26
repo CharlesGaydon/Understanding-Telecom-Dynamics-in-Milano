1315b9f7fa61af616f93881f1822668f30a9f192
CharlesGaydon <charles@gaydon.fr>
2017-11-26 19:22:11 +0100

    Date processing for later grouping added. Jupyter notebook with examples of visualisation.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Understanding Telecom Dynamics in Milano
+
+Italians are known to be quite talkative. What can their telecommunications teach us about the dynamic of Milano ?
+
+Here is the link of the hackmd : https://hackmd.io/GwMwxsCMBGkKwFoCcITQQFgMwY8kwwCAHAIYECm0ADNCBgCbRA==
+A enlever quand on passera public evidemment.
+
+usage :
+
+Pour créer l'environnement anaconda, utiliser : 
+	
+	conda env create --file conda_Milano_env.yml
+puis l'activer (source si sous windows):
+	
+	(source) activate Milano
--- preprocess_tools.py
+++ preprocess_tools.py
@@ -9,46 +9,71 @@ Last edit : 20/11/2017
 import pandas as pd 
 import sys
 import numpy as np
+import datetime
 
 def geo_filter_a_MI(file, fileout):
-  """
+    """
     Take a Milano file, name its columns, filter by square id the lines, 
     fill the NaN value with 0, and save.
-  """
+    output : 
+    ['Square','Time','Country','SMSin','SMSout','Callin','Callout',
+    'Internet',"Month","Day","Hour","WDay"]
+    """
     d = pd.read_csv(file, sep = '\t',header=None)
+    
     d.columns = ['Square','Time','Country','SMSin','SMSout','Callin','Callout','Internet']
+
     mask = d["Square"].apply(keep_a_square)
     d = d[mask]
     d = d.fillna(0)
-    d.to_csv(path_or_buf = fileout, sep = '\t', index = False)
+
+    t = pd.DataFrame(data=d['Time'].apply(unix_to_ints))
+    d['Month'] = t["Time"].apply(lambda x : x[0])
+    d['Day'] = t["Time"].apply(lambda x : x[1])
+    d['Hour'] = t["Time"].apply(lambda x : x[2])
+    d['WDay'] = t["Time"].apply(lambda x : x[3])
+    d.to_csv(path_or_buf = fileout, sep = '\t', index = False, header=True)
     print("Saved to : "+ fileout)
     return(d)
 
 def geo_filter_a_MI2MI(file, fileout):
-  """
+    """
     Take a Milano 2 Milano file, name its columns, filter by square id the lines, 
     fill the NaN value with 0, and save.
-  """
+    ['Id1','Id2','Time','Signal',
+    "Month","Day","Hour","WDay"]
+    """
     d = pd.read_csv(file, sep = '\t',header=None)
     d.columns = ['Id1','Id2','Time','Signal']
     mask = d['Id1'].apply(keep_a_square) and d['Id2'].appply(keep_a_square)
     d = d[mask]
     d.fillna(0)
+    t = pd.DataFrame(data=d['Time'].apply(unix_to_ints))
+    d['Month'] = t["Time"].apply(lambda x : x[0])
+    d['Day'] = t["Time"].apply(lambda x : x[1])
+    d['Hour'] = t["Time"].apply(lambda x : x[2])
+    d['WDay'] = t["Time"].apply(lambda x : x[3])
     d.to_csv(path_or_buf = fileout, sep = '\t', index = False)
     print("Saved to : "+ fileout)
     return(d)
 
 def keep_a_square(x,xmin=20,xmax=89,ymin=15,ymax=85):
-  """
+    """
     Returns True if a square is within our chosen sub-grid.
-  """
-  j = x%100
-  if j > xmax or j < xmin:
-    return(False)
-  i = (x-j)/100
-  if i <ymin or i > ymax:
-    return(False)
-  return(True)
+    """
+    j = x%100
+    if j > xmax or j < xmin:
+        return(False)
+    i = (x-j)/100
+    if i <ymin or i > ymax:
+        return(False)
+    return(True)
+
+def unix_to_ints(unix_code):
+    tstamp = datetime.datetime.utcfromtimestamp(unix_code/1000)
+    mdH = tstamp.strftime('%m-%d-%H').split('-')
+    mdH.append(tstamp.weekday())
+    return(list(map(int,mdH)))
 
 if __name__ == "__main__":
     if len(sys.argv)<2:
