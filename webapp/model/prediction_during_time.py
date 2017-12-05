d5427a8209c1999219d4a3deda6bdaee24f73185
Enzo Lebrun <enzo.the@gmail.com>
2017-12-05 16:52:27 +0100

    adding a way to get date from multiple files

--- webapp/model/prediction_during_time.py
+++ webapp/model/prediction_during_time.py
@@ -1,10 +1,24 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import glob
+import os
 
-def get_callin_during_time(filename):
+
+
+def get_multiple_df(path="data/"):
+    allFiles = glob.glob(os.getcwd() + "/" + path + "*.txt")
+    frame = pd.DataFrame()
+    list_ = []
+    for file_ in allFiles:
+        df = pd.read_csv(file_, sep='\t', header=None)
+        list_.append(df)
+    return pd.concat(list_)
+    
+
+def get_callin_during_time(path="data/"):
     # take a file and return an dataframe with column1(time) and column2(callin)
-    df = pd.read_csv(filename, sep = '\t', header=None)
+    df = get_multiple_df()
     df.columns = ['Square','Time','Country','SMSin','SMSout','Callin','Callout','Internet']
     return df.fillna(0)[['Time', 'Callin']].groupby('Time', sort=True).mean()
 
